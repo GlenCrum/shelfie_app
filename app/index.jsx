c675fc47317605b92f0c9e49ee294a883f93c0dc
Glen Crum <glen.m.crum@gmail.com>
2025-08-18 19:31:33 +1000

    Added some more text, added images for Logo and added image source. Updates to styles.

--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,37 @@
-import { Text, StyleSheet, View } from 'react-native'
+import { Text, StyleSheet, View, Image } from 'react-native'
+import Logo from '../assets/Image/logo_light.png'
 
  const Home = () => {
     return (
-        <View> 
-            <Text>Home</Text>
+        <View style={styles.container}> 
+            <Image source={Logo} style={styles.img} />
+
+            <Text style={styles.title}>Number 1</Text>
+
+            <Text style={{marginTop: 10, marginBottom: 30}}>
+                Reading List App
+                </Text>
+
         </View>
     )
  }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 18,
+
+    },
+
+    img: {
+        marginVertical: 20,
+    },
+})
